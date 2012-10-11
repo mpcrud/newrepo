4a4da9f42a332d36034c01430efc14b247b9f28e
miti <smriti3008@gmail.com>
2012-10-11 16:26:46 +0530

    files

---
+++ src/main/java/com/app/mpadmin/web/util/ExceptionUtil.java
@@ -0,0 +1,16 @@
+package com.app.mpadmin.web.util;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+/**
+ * Use it during development to throw exceptions from your flows and thus verify that your
+ * error handling politic is correct.
+ */
+@Named
+@Singleton
+public class ExceptionUtil {
+    public void throwRuntimeException() {
+        throw new RuntimeException("Just testing from ExceptionUtil ...");
+    }
+}
