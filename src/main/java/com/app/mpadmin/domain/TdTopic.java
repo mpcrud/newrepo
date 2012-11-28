6ffc01b6ea4e5b2d82c7bf957beac26204d90714
miti <smriti3008@gmail.com>
2012-11-29 03:22:41 +0530

    tdtopic

--- src/main/java/com/app/mpadmin/domain/TdTopic.java
+++ src/main/java/com/app/mpadmin/domain/TdTopic.java
@@ -542,7 +542,7 @@ public class TdTopic implements Identifiable<Integer>, Serializable {
     public String toString() {
         return Objects.toStringHelper(this) //
                 .add("topicId", getTopicId()) //
-                .add("topicTitle", getTopicTitle().replace("&nbsp;"," ").substring(0,15)) //
+                .add("topicTitle", getTopicTitle().replace("&nbsp;"," ").substring(0,10)) //
               /*  .add("viewCount", getViewCount()) //
                 .add("commentCount", getCommentCount()) //
                 .add("assignedSince", getAssignedSince()) //
